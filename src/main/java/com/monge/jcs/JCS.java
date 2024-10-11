25fd1442d7b546210177af0da8f28203bb919c11
monge.686 <64815095+herzank1@users.noreply.github.com>
2024-10-11 08:53:52 -0700

    Add files via upload

---
+++ src/main/java/com/monge/jcs/JCS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.monge.jcs;
+
+/**
+ *
+ * @author HP
+ */
+public class JCS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
